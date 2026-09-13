efd740f2b466038778c2f092ccbedfb964c8d12a
MD. Janey Alom Sunny <md.j.a.sunny@outlook.com>
2026-09-13 14:46:42 +0600

    Populated sources.nix

--- sources.nix
+++ sources.nix
@@ -1,21 +1,21 @@
 {
-  version = "0.0.0";
-  modDirVersion = "0.0.0-current-sunxi64";
+  version = "6.18.51";
+  modDirVersion = "6.18.51-current-sunxi64";
 
   image = {
-    url = "https://beta.armbian.com/PLACEHOLDER";
-    sha256 = "0000000000000000000000000000000000000000000000000000";
+    url = "https://beta.armbian.com/pool/main/l/linux-6.18.51/linux-image-current-sunxi64_26.11.0-trunk.48_arm64__6.18.51-Sf638-D32de-P2fdb-C4e0c-H6651-HK01ba-Vc222-Bc768-R448a.deb";
+    sha256 = "0byknz66dxp7za21a7fq7w5llv78s05xrjbjpdvs9jhpgk407j4i";
   };
   dtb = {
-    url = "https://beta.armbian.com/PLACEHOLDER";
-    sha256 = "0000000000000000000000000000000000000000000000000000";
+    url = "https://beta.armbian.com/pool/main/l/linux-dtb-current-sunxi64/linux-dtb-current-sunxi64_26.11.0-trunk.48_arm64__6.18.51-Sf638-D32de-P2fdb-C4e0c-H6651-HK01ba-Vc222-Bc768-R448a.deb";
+    sha256 = "1f18ca6vpc05n7p5dy2a5463nwv97as6wg0201r78qmpgbjkwrli";
   };
   headers = {
-    url = "https://beta.armbian.com/PLACEHOLDER";
-    sha256 = "0000000000000000000000000000000000000000000000000000";
+    url = "https://beta.armbian.com/pool/main/l/linux-headers-current-sunxi64/linux-headers-current-sunxi64_26.11.0-trunk.48_arm64__6.18.51-Sf638-D32de-P2fdb-C4e0c-H6651-HK01ba-Vc222-Bc768-R448a.deb";
+    sha256 = "1y94hkdm2w96579jhpayhfgamvp1470mr3mhh497rq8y7yib23b4";
   };
   libcDev = {
-    url = "https://beta.armbian.com/PLACEHOLDER";
-    sha256 = "0000000000000000000000000000000000000000000000000000";
+    url = "https://beta.armbian.com/pool/main/l/linux-libc-dev-current-sunxi64/linux-libc-dev-current-sunxi64_26.11.0-trunk.48_arm64__6.18.51-Sf638-D32de-P2fdb-C4e0c-H6651-HK01ba-Vc222-Bc768-R448a.deb";
+    sha256 = "07y4y9547jc9p9ib5phcrrznaffiwfxx8rw8n3zrgh3imkaj3har";
   };
 }
