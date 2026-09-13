da03efa07bfb67a6977ea07f0d8b3167801dacd4
MD. Janey Alom Sunny <md.j.a.sunny@outlook.com>
2026-09-13 16:27:25 +0600

    Add sources-edge.nix for Armbian kernel version 7.2.5

---
+++ sources-edge.nix
@@ -0,0 +1,21 @@
+{
+  version = "7.2.5";
+  modDirVersion = "7.2.5-edge-sunxi64";
+
+  image = {
+    url = "https://beta.armbian.com/pool/main/l/linux-7.2.5/linux-image-edge-sunxi64_26.11.0-trunk.48_arm64__7.2.5-Sa300-D32de-Pdf7b-C8579-H8cb1-HK01ba-Vc222-Bc768-R448a.deb";
+    sha256 = "0lpx76z1d2v63w9mag9j6a14iy484a9qya2zimclkwndm3fm3la9";
+  };
+  dtb = {
+    url = "https://beta.armbian.com/pool/main/l/linux-dtb-edge-sunxi64/linux-dtb-edge-sunxi64_26.11.0-trunk.48_arm64__7.2.5-Sa300-D32de-Pdf7b-C8579-H8cb1-HK01ba-Vc222-Bc768-R448a.deb";
+    sha256 = "0j2v7slk6aw7rm1xzycdli1l8jzphg6bpv3cykqfnpz6drad0dgy";
+  };
+  headers = {
+    url = "https://beta.armbian.com/pool/main/l/linux-headers-edge-sunxi64/linux-headers-edge-sunxi64_26.11.0-trunk.48_arm64__7.2.5-Sa300-D32de-Pdf7b-C8579-H8cb1-HK01ba-Vc222-Bc768-R448a.deb";
+    sha256 = "0p7mrf3kh5k2czvhyn86jjkfmwvz4k0q47sqk31bpa6kjblhjymw";
+  };
+  libcDev = {
+    url = "https://beta.armbian.com/pool/main/l/linux-libc-dev-edge-sunxi64/linux-libc-dev-edge-sunxi64_26.11.0-trunk.48_arm64__7.2.5-Sa300-D32de-Pdf7b-C8579-H8cb1-HK01ba-Vc222-Bc768-R448a.deb";
+    sha256 = "06wd3zh1aqn09b36ga9dzd0jv6iw3l0lmazli3fdlhcs4grjkis7";
+  };
+}
